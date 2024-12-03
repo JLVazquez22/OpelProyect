fea503287484ba407572e7e85f368c607ea727de
JLVazquez22 <vazaceitunojoseluis@gmail.com>
2024-12-03 12:31:50 +0100

    Create nav hamburguer, fixed and edit errors

--- src/script.js
+++ src/script.js
@@ -1,10 +1,18 @@
 let index = 0;
 
+// FUNCION CARRUSEL
 function moveCarousel(direction) {
     const carousel = document.getElementById('carousel');
     const totalItems = carousel.children.length;
-    
-    // Cambiar a 4 elementos
     index = (index + direction + totalItems) % totalItems;
     carousel.style.transform = `translateX(-${index * 100}%)`;
 }
+
+// FUNCION MENÚ
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menu-icon');
+    const menu = document.getElementById('menu');
+    menuIcon.addEventListener('click', () => {
+        menu.classList.toggle('hidden'); 
+    });
+});
